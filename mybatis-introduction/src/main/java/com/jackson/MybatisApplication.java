636299b306157295e7aaef70fce5b6e60f5e45cf
xuyangyang <yangyang.xu@aqara.com>
2020-07-31 15:49:03 +0800

    mybatis框架源码解读

--- mybatis-introduction/src/main/java/com/jackson/MybatisApplication.java
+++ mybatis-introduction/src/main/java/com/jackson/MybatisApplication.java
@@ -8,6 +8,9 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Hello world!
@@ -19,7 +22,11 @@ public class MybatisApplication {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        User user = userMapper.selectById(1L);
+        User user = userMapper.selectById(new HashMap<String, Object>() {{
+            put("id", 1L);
+        }});
+        List<User> users = userMapper.selectByIdCollection(Arrays.asList(1L, 2L));
+        users.forEach(System.out::println);
         System.out.println(user);
     }
 }
--- mybatis-introduction/src/main/java/com/jackson/mapper/UserMapper.java
+++ mybatis-introduction/src/main/java/com/jackson/mapper/UserMapper.java
@@ -1,9 +1,14 @@
 package com.jackson.mapper;
 
 import com.jackson.entity.User;
-import org.apache.ibatis.annotations.Param;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
 
-    User selectById(@Param("id") Long id);
+    User selectById(Map<String, Object> paramMap);
+
+    List<User> selectByIdCollection(Collection<? extends Long> collection);
 }
