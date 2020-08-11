12ef34fa73b086f1df86b3591efd732fff1c6f9c
xuyangyang <yangyang.xu@aqara.com>
2020-08-11 15:26:16 +0800

    spring boot dubbo源码解读

---
+++ spring-boot/src/test/java/com/jackson/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.jackson;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
